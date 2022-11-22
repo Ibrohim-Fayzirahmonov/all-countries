7ab50577981cd9efca36812821562970fa2e9d6d
“Ibroxim-Fayziraxmonov” <fayzirakhmonov9326@gmail.com>
2022-11-23 00:53:55 +0500

    lesson 7

---
+++ js/main.js
@@ -0,0 +1,120 @@
+"use strict";
+const baseURL = "https://restcountries.com/v2";
+
+// -------------- ALL COUNTRIES --------------------------- //
+const getAllCountries = async () => {
+    const countries = await fetch(`${baseURL}/all`);
+    const result = await countries.json();
+    dataRender(result)
+    dynamicCategory(result)
+}
+
+getAllCountries()
+
+// -------------- ALL COUNTRIES END --------------------------- //
+
+
+
+// ------------ RENDER ALL DATA ----------------------- //
+
+function dataRender(data = []) {
+
+    data.forEach((el) => {
+
+        const card = createElement('div', 'card shadow-lg', `
+        <img src="${el.flags.svg ? el.flags.svg : "https://picsum.photos/id/122/267/160"}" alt="flag" class="card-top-img">
+        <div class="card-body p-4">
+        <h3 class="card-title">${el.name}</h3>
+        <ul class="card-list p-0">
+        <li class="card-list-item list-unstyled"><strong>Population: </strong> ${el.population} </li>
+        <li class="card-list-item list-unstyled"><strong>Region: </strong> ${el.region} </li>
+        <li class="card-list-item list-unstyled"><strong>Capital: </strong> ${el.capital} </li>
+        </ul>
+        </div>`);
+
+        $(".wrapper").appendChild(card);
+
+    })
+}
+
+// ------------ RENDER ALL DATA END ----------------------- //
+
+
+// -------------- dynamicCategory ----------------- //
+
+function dynamicCategory(data) {
+
+    const category = [];
+
+    data.forEach((el) => {
+        if (!category.includes(el.region)) {
+            category.push(el.region)
+        }
+    })
+
+    category.sort()
+    category.unshift('All');
+    category.forEach((el) => {
+        const option = createElement('option', 'item', el);
+        $('#region').appendChild(option)
+    })
+}
+
+// -------------- dynamicCategory END ----------------- //
+
+$("#search").addEventListener('keypress', (e) => {
+    if (e.target.value.trim().length !== 0 && e.keyCode === 13) {
+        findCountry(e.target.value);
+    }
+})
+
+// -------------------- FIND COUNTRIES ----------------- //
+
+async function findCountry(country) {
+
+    $('.wrapper').innerHTML = "";
+    const response = await fetch(`${baseURL}/name/${country}`);
+
+    const data = await response.json();
+    if (response.status === 404) {
+        $('.info').innerHTML = "<h1 class='text-center w-100 mb-5'>Please enter the name of country</h1>"
+    } else {
+        $('.info').innerHTML = `<h1 class='text-center w-100 mb-5'>Qidiruv natijasi:${data.length}</h1>`;
+        dataRender(data)
+    }
+}
+
+// ------------------- FIND COUNTRIES END ------------------- //
+
+$('#region').addEventListener('change', (e) => {
+    sortCountry(e.target.value.toLowerCase())
+})
+
+
+async function sortCountry(region) {
+    $('.wrapper').innerHTML = "";
+    if (region === "all") {
+        const response = await fetch(`${baseURL}/all`);
+
+        const data = await response.json();
+
+        if (response.status === 404) {
+            $('.info').innerHTML = "<h1 class='text-center w-100'>Please enter the name of country</h1>"
+        } else {
+
+            $('.info').innerHTML = `<h1 class='text-center w-100 mb-5'>Qidiruv natijasi:${data.length}</h1>`;
+            dataRender(data)
+        }
+    } else {
+        const response = await fetch(`${baseURL}/region/${region}`);
+
+        const data = await response.json();
+
+        if (response.status === 404) {
+            $('.info').innerHTML = "<h1 class='text-center w-100'>Please enter the name of country</h1>"
+        } else {
+            $('.info').innerHTML = `<h1 class='text-center w-100 mb-5'>Qidiruv natijasi:${data.length}</h1>`;
+            dataRender(data)
+        }
+    }
+}
